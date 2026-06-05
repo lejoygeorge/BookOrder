1db53325455e33e4264a6c64bd973b7eef745643
Lejoy George <lejoygeorge@Lejoys-MacBook-Air.local>
2026-06-05 15:24:07 +0530

    Added BookDiscountUtilsTest

---
+++ src/test/java/com/bookorder/demo/service/BookDiscountUtilsTest.java
@@ -0,0 +1,17 @@
+package com.bookorder.demo.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BookDiscountUtilsTest {
+
+    @Test
+    void testGroupPricesAreCalculatedCorrectly() {
+        assertEquals(50.0, BookDiscountUtils.GROUP_PRICE.get(1), 0.001, "Price for 1 book should be 50.0");
+        assertEquals(95.0, BookDiscountUtils.GROUP_PRICE.get(2), 0.001, "Price for 2 books should be 95.0");
+        assertEquals(135.0, BookDiscountUtils.GROUP_PRICE.get(3), 0.001, "Price for 3 books should be 135.0");
+        assertEquals(160.0, BookDiscountUtils.GROUP_PRICE.get(4), 0.001, "Price for 4 books should be 160.0");
+        assertEquals(187.5, BookDiscountUtils.GROUP_PRICE.get(5), 0.001, "Price for 5 books should be 187.5");
+    }
+}
